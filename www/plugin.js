5b653c67bc9f8a2ba2ec4fa97dc0bef69877e84e
redoc-n <redoc123@users.noreply.github.com>
2018-01-04 13:04:36 +0530

    made all related attributes into options array

--- www/plugin.js
+++ www/plugin.js
@@ -5,12 +5,12 @@ var PLUGIN_NAME = 'PaytmPlugin';
 
 var PaytmPlugin = {
    //orderid, cust_id, email, phone, txn_amt,callback_url,checksum_hash,environment
-  payWithPaytm: function(orderId, customerId, email, phone, amount,callbackUrl,checksum, method, successCallback, failureCallback) {
+  payWithPaytm: function(options, successCallback, failureCallback) {
     exec(successCallback,
                  failureCallback,
                  PLUGIN_NAME,
                  "payWithPaytm",
-                 [orderId, customerId, email, phone, amount, callbackUrl, checksum, method]);
+                 options);
   }
 
 
